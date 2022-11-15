e8f2fb8e7fc33c549cb24e2803caa7d4ca5aa156
Huy Bui <quochuy270800@gmail.com>
2022-11-15 15:59:27 +0200

    add sign up and sign in functionalities

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { toASCII } from 'punycode';
 import { AppModule } from './app/app.module';
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist:true
+  }));
   await app.listen(3000);
   if (module.hot) {
     module.hot.accept();
