f36d742eb98a345151f8b599b3bf2bba4f35d6b2
quochuy278 <quochuy270800@gmail.com>
2023-02-26 21:07:25 +0200

    add config for response Swagger in Business

--- src/business/business.controller.ts
+++ src/business/business.controller.ts
@@ -1,15 +1,29 @@
 import { Body, Controller, Get, Param, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiProperty,
+  ApiResponse,
+} from '@nestjs/swagger';
 import { Exclude } from 'class-transformer';
 import { JwtGuard } from 'src/auth/guard';
 import { OrderingIoService } from 'src/ordering.io/ordering.io.service';
 import { BusinessAttributes } from 'src/type';
 
 import { UtilsService } from 'src/utils/utils.service';
+import { AllBusinessDto, BusinessDto } from './dto/business.dto';
 
 @UseGuards(JwtGuard)
 @Controller('business')
+@ApiBadRequestResponse({
+  description: 'Something wrong happened',
+})
 export class BusinessController {
   constructor(private utils: UtilsService, private orderingIo: OrderingIoService) {}
+  @ApiCreatedResponse({
+    description: 'Get all businesses',
+    type: AllBusinessDto,
+  })
   @Post('allbusiness')
   async getAllBusiness(@Request() req: any, @Body('publicUserId') publicUserId: string) {
     const { userId } = req.user;
@@ -19,34 +33,49 @@ export class BusinessController {
     const accessToken = await this.utils.getAccessToken(userId);
     return this.orderingIo.getAllBusiness(accessToken, publicUserId);
   }
-  @Get(':businessId')
-  async getBusinessById(@Param('businessId') businessId: number, @Request() req) {
+  @ApiCreatedResponse({
+    description: 'Get a specific business',
+    type: BusinessDto,
+  })
+  @Post('findBusiness')
+  async getBusinessById(
+    @Request() req: any,
+    @Body('publicBusinessId') publicBusinessId: string,
+  ) {
     const { userId } = req.user;
     const accessToken = await this.utils.getAccessToken(userId);
-    return this.orderingIo.getBusinessById(businessId, accessToken);
+    return this.orderingIo.getBusinessById(publicBusinessId, accessToken);
   }
+  @ApiCreatedResponse({
+    description: 'Edit a specific business',
+    type: BusinessDto,
+  })
   @Post('editBusiness')
   async editBusiness(
     @Request() req: any,
     @Body('publicBusinessId') publicBusinessId: string,
     @Body('status') status: boolean,
-
   ) {
     const { userId } = req.user;
     const accessToken = await this.utils.getAccessToken(userId);
-    console.log(status)
-    return this.orderingIo.editBusiness( accessToken,publicBusinessId, status);
+    console.log(status);
+    return this.orderingIo.editBusiness(accessToken, publicBusinessId, status);
   }
+  @ApiCreatedResponse({
+    description: 'Activate business',
+    type: BusinessDto,
+  })
   @Post('editBusiness/activate')
-  async activateBusiness(
-    @Request() req: any,
-    @Body('publicBusinessId') publicBusinessId: string,
-  ) {
+  async activateBusiness(@Request() req: any, @Body('publicBusinessId') publicBusinessId: string) {
     const { userId } = req.user;
     const accessToken = await this.utils.getAccessToken(userId);
 
-    return this.orderingIo.activateBusiness( accessToken,publicBusinessId);
+    return this.orderingIo.activateBusiness(accessToken, publicBusinessId);
   }
+  @ApiCreatedResponse({
+    description: 'Deactivate business',
+    type: BusinessDto,
+  })
   @Post('editBusiness/deactivate')
   async deactivateBusiness(
     @Request() req: any,
@@ -55,6 +84,6 @@ export class BusinessController {
     const { userId } = req.user;
     const accessToken = await this.utils.getAccessToken(userId);
 
-    return this.orderingIo.deactivateBusiness( accessToken,publicBusinessId);
+    return this.orderingIo.deactivateBusiness(accessToken, publicBusinessId);
   }
 }
