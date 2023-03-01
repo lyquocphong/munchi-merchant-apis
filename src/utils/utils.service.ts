4ff761f5d8fb280f772892c6387ee62bc6346383
quochuy278 <quochuy270800@gmail.com>
2023-03-01 13:03:26 +0200

    remove getUser and createUser  and user service

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -86,85 +86,6 @@ export class UtilsService {
       this.getError(error);
     }
   }
-  async getUser(userId: number, publicUserId: string) {
-    if (userId === null) {
-      const userByPublicUserId = await this.prisma.user.findUnique({
-        where: {
-          publicId: publicUserId,
-        },
-        select: {
-          userId: true,
-          email: true,
-          firstName: true,
-          lastname: true,
-          publicId: true,
-          level: true,
-          business: {
-            select: {
-              publicId: true,
-              name: true,
-            },
-          },
-          session: {
-            select: {
-              accessToken: true,
-              expiresIn: true,
-              tokenType: true,
-            },
-          },
-        },
-      });
-      const verifyToken = await this.signToken(userByPublicUserId.userId, userByPublicUserId.email);
-      const userResponseByPublicUserId = new UserResponse(
-        userByPublicUserId.email,
-        userByPublicUserId.firstName,
-        userByPublicUserId.lastname,
-        userByPublicUserId.level,
-        userByPublicUserId.publicId,
-        userByPublicUserId.session,
-        verifyToken.verifyToken,
-      );
-      return userResponseByPublicUserId;
-    } else {
-      const userByUserId = await this.prisma.user.findUnique({
-        where: {
-          userId: userId,
-        },
-        select: {
-          userId: true,
-          email: true,
-          firstName: true,
-          lastname: true,
-          publicId: true,
-          level: true,
-          business: {
-            select: {
-              publicId: true,
-              name: true,
-            },
-          },
-          session: {
-            select: {
-              accessToken: true,
-              expiresIn: true,
-              tokenType: true,
-            },
-          },
-        },
-      });
-      const verifyToken = await this.signToken(userByUserId.userId, userByUserId.email);
-      const userResponseByPublicUserId = new UserResponse(
-        userByUserId.email,
-        userByUserId.firstName,
-        userByUserId.lastname,
-        userByUserId.level,
-        userByUserId.publicId,
-        userByUserId.session,
-        verifyToken.verifyToken,
-      );
-      return userResponseByPublicUserId;
-    }
-  }
 
   async getUpdatedPublicId(publicUserId: string) {
     const newPublicUserId = this.getPublicId();
@@ -183,54 +104,7 @@ export class UtilsService {
     const publicId = uuidv4();
     return publicId;
   }
-  async createUser(data: any, password: string) {
-    const encryptedPassword = this.getPassword(password, true);
-    const { access_token, token_type, expires_in } = data.session;
-    const verifyToken = await this.signToken(data.userId, data.email);
-    const newUser = await this.prisma.user.create({
-      data: {
-        userId: data.id,
-        firstName: data.name,
-        lastname: data.lastname,
-        email: data.email,
-        hash: encryptedPassword,
-        level: data.level,
-        publicId: this.getPublicId(),
-        session: {
-          create: {
-            accessToken: access_token,
-            expiresIn: expires_in,
-            tokenType: token_type,
-          },
-        },
-      },
-      select: {
-        firstName: true,
-        lastname: true,
-        email: true,
-        publicId: true,
-        level: true,
-        session: {
-          select: {
-            accessToken: true,
-            expiresIn: true,
-            tokenType: true,
-          },
-        },
-      },
-    });
-    const userResponse = new UserResponse(
-      newUser.email,
-      newUser.firstName,
-      newUser.lastname,
-      newUser.level,
-      newUser.publicId,
-      newUser.session,
-      verifyToken.verifyToken,
-    );
-    return userResponse;
-  }
-
+  
   getPassword(password: string, needCrypt: boolean) {
     const cryptr = new Cryptr(this.config.get('HASH_SECRET'));
     let passwordAfter: string;
