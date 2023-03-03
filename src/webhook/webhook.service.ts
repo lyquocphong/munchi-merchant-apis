4c625ab02f22d41647afa2b103e4604372ff1236
quochuy278 <quochuy270800@gmail.com>
2023-03-03 21:38:19 +0200

    add websocket for newOrder route

--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -16,23 +16,13 @@ import { BusinessService } from 'src/business/business.service';
 
 @WebSocketGateway({ cors: { origin: 'http://localhost:3000' } })
 @Injectable()
-export class OrderWebhookService {
+export class WebhookService {
   constructor( private business: BusinessService) {}
   @WebSocketServer() server: Server;
   onModuleInit() {
     const ioServer = this.server;
     ioServer.on('connection', async (socket) => {
-      // const publicUserId = socket.handshake.auth.publicUserId;
       console.log(`${socket.id} connected`);
-      // console.log(socket.handshake.auth.publicUserId);
-      // const userId = socket.handshake.auth.publicUserId;
-      // if (userId) {
-      //   const user = await this.user.getUser(null, socket.handshake.auth.publicUserId);
-      //   socket.to(userId).emit('private message');
-      //   console.log(user);
-      // } else {
-      //   console.log('Loading');
-      // }
       console.log(socket.rooms);
       socket.on('join', (room) => {
         socket.join(room);
@@ -42,29 +32,6 @@ export class OrderWebhookService {
       console.log(socket.rooms, 'line 42');
     });
   }
-  // OnGatewayConnection() {
-  //   this.server.use(async (socket, next) => {
-  //     const publicUserId = socket.handshake.auth.publicUserId;
-  //     if (publicUserId) {
-  //       // find existing session
-  //       const user = await this.user.getUser(null, publicUserId);
-  //       if (user) {
-  //         console.log(user, 'line 35 middleware ');
-  //         return next();
-  //       }
-  //     }
-
-  //     // const username = socket.handshake.auth.username;
-  //     // if (!username) {
-  //     //   return next(new Error("invalid username"));
-  //     // }
-  //     // // create new session
-  //     // socket.sessionID = randomId();
-  //     // socket.userID = randomId();
-  //     // socket.username = username;
-  //     next();
-  //   });
-  // }
  async newOrder(order:any) {
     console.log(order.businessId)
     const business = await this.business.getBusinessById(order.businessId)
