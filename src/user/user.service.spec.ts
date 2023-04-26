305276ef90d0f754a52ce4e60bf7e4ba8fa0a52e
quochuy278 <quochuy270800@gmail.com>
2023-04-26 16:03:52 +0300

    add testing for user service

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
-
+import { UserController } from './user.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UtilsService } from 'src/utils/utils.service';
 describe('UserService', () => {
   let service: UserService;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
-    }).compile();
+      providers: [
+        {
+          provide: UserService,
+          useValue: { PrismaService, UtilsService, getUserByPublicId: jest.fn() },
+        },
+      ],
+    })
+      .compile();
 
     service = module.get<UserService>(UserService);
   });
-
-  it('should be defined', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+  it('user service should be defined', () => {
     expect(service).toBeDefined();
+  });
+});
+
+describe('User Controller', () => {
+  let controller: UserController;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [{ provide: UserController, useValue: {} }],
+    }).compile();
+    controller = module.get<UserController>(UserController);
+  });
+
+  it('user controller should be defined', () => {
+    expect(controller).toBeDefined();
   });
 });
