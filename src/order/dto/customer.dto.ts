9b5995405ba1f4bede58ea760df229a5872b75d7
quochuy278 <quochuy270800@gmail.com>
2023-05-21 14:11:49 +0300

    add order history , cell phone attribute to orderdto and fix customerdto

--- src/order/dto/customer.dto.ts
+++ src/order/dto/customer.dto.ts
@@ -4,12 +4,22 @@ import { Exclude, Expose } from 'class-transformer';
 export class CustomerDto {
   @Expose()
   name: number;
-  @Expose({name:'lastname'})
+  
+  @Expose({ name: 'lastname' })
   lastName: number;
+
   @Expose()
   email: number;
+
   @Expose()
-  address: number;
+  address: string;
+
+  @Expose({ name: 'address_notes' })
+  addressNotes: string;
+  
+  @Expose({ name: 'cellphone' })
+  phone: string;
+
   constructor(partial: Partial<CustomerDto>) {
     Object.assign(this, partial);
   }
