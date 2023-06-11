27e9f3de33868e8f2a630c57203787c06ea11b69
quochuy278 <quochuy270800@gmail.com>
2023-06-11 14:10:01 +0300

    add signOut, extent token duration, add createSession method upon signIn

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,48 +1,62 @@
-import { Controller, Post, Body, UseGuards, Get, Request } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiCreatedResponse } from '@nestjs/swagger';
-import { OrderingIoService } from 'src/ordering.io/ordering.io.service';
-import { AuthCredentials } from 'src/type';
-import { AuthService } from './auth.service';
-import { AuthReponseDto } from './dto/auth.dto';
-import { RefreshJwt } from './guard/refreshJwt.guard';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Get,
+  Request,
+} from "@nestjs/common";
+import { ApiBadRequestResponse, ApiCreatedResponse } from "@nestjs/swagger";
+import { OrderingIoService } from "src/ordering.io/ordering.io.service";
+import { AuthCredentials } from "src/type";
+import { AuthService } from "./auth.service";
+import { AuthReponseDto } from "./dto/auth.dto";
+import { RefreshJwt } from "./guard/refreshJwt.guard";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
-  constructor(private orderingIo: OrderingIoService, private auth: AuthService) {}
+  constructor(
+    private orderingIo: OrderingIoService,
+    private auth: AuthService
+  ) {}
   @ApiCreatedResponse({
-    description: 'Sign up new user',
+    description: "Sign up new user",
     type: AuthReponseDto,
   })
   @ApiBadRequestResponse({
-    description: 'Please try again',
+    description: "Please try again",
   })
-  @Post('signup')
+  @Post("signup")
   signup(@Body() credentials: AuthCredentials) {
     return this.orderingIo.signUp(credentials);
   }
+
   @ApiCreatedResponse({
-    description: 'Sign in new user',
+    description: "Sign in new user",
     type: AuthReponseDto,
   })
   @ApiBadRequestResponse({
-    description: 'Please try again',
+    description: "Please try again",
   })
-  @Post('signin')
-  signin(@Body() credentials: AuthCredentials) {
+  @Post("signin")
+  async signin(@Body() credentials: AuthCredentials) {
     return this.orderingIo.signIn(credentials);
   }
+
   @ApiCreatedResponse({
-    description: 'Signed out',
+    description: "Signed out",
   })
-  @Post('signout')
-  signout(@Body('publicUserId') publicUserId: string) {
+  @Post("signout")
+  async signout(@Body("publicUserId") publicUserId: string) {
+ 
+    await this.auth.signOut(publicUserId);
     return this.orderingIo.signOut(publicUserId);
   }
   @UseGuards(RefreshJwt)
-  @Get('refreshToken')
+  @Get("refreshToken")
   getRefreshTokens(@Request() req: any) {
-    const userId = req.user['sub'];
-    const refreshToken = req.user['refreshToken'];
-    return this.auth.refreshTokens(userId,refreshToken);
+    const userId = req.user["sub"];
+    const refreshToken = req.user["refreshToken"];
+    return this.auth.refreshTokens(userId, refreshToken);
   }
 }
