612af6e895cedd1a5c92387ce27889bb4ed935c3
quochuy278 <quochuy270800@gmail.com>
2023-06-13 14:19:40 +0300

    add cms module to app

---
+++ src/cms/cms.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { OrderingIoService } from 'src/ordering.io/ordering.io.service';
+import { UtilsService } from 'src/utils/utils.service';
+
+@Injectable()
+export class CmsService {
+  constructor(private utils: UtilsService, private orderingIoService: OrderingIoService) {}
+  async getPage(userId: number) {
+    const accessToken = await this.utils.getAccessToken(userId);
+    // getPage from ordering
+    return await this.orderingIoService.getPage(accessToken);
+    //return getPage to client
+  }
+}
