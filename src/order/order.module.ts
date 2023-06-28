bc5b77683630a0afed5104bbf25d721a16adb81a
Huy Bui <quochuy270800@gmail.com>
2023-06-28 18:36:59 +0300

    refactor order service by moving order related  method  with ordering back to order service file

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -3,10 +3,13 @@ import { JwtStrategy } from 'src/auth/strategy/jwt.strategy';
 import { OrderingIoModule } from 'src/ordering.io/ordering.io.module';
 import { UtilsModule } from 'src/utils/utils.module';
 import { OrderController } from './order.controller';
+import { OrderService } from './order.service';
+import { BusinessModule } from 'src/business/business.module';
 
 @Module({
-  imports: [UtilsModule, OrderingIoModule],
+  imports: [UtilsModule, OrderingIoModule, BusinessModule],
   controllers: [OrderController],
-  providers: [JwtStrategy],
+  providers: [JwtStrategy, OrderService],
+  exports: [OrderService],
 })
 export class OrderModule {}
