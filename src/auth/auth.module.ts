b1fc7bc17801aebcca4754ebf30ae042c1febca3
Huy Bui <quochuy270800@gmail.com>
2023-06-29 15:12:43 +0300

    add session service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,11 +5,16 @@ import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { RefreshTokenStrategy } from './strategy/refreshJwt.strategy';
+import { SessionService } from './session.service';
 
 @Module({
-  imports: [JwtModule.register({}), forwardRef(() => UserModule), OrderingIoModule],
+  imports: [
+    JwtModule.register({}),
+    forwardRef(() => UserModule),
+    forwardRef(() => OrderingIoModule),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, RefreshTokenStrategy],
-  exports: [AuthService],
+  providers: [AuthService, RefreshTokenStrategy, SessionService],
+  exports: [AuthService, SessionService],
 })
 export class AuthModule {}
