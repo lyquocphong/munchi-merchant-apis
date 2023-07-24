629b7cd5c30b820fb796b393568237d3350551a8
Phong Ly <lyquocphong@gmail.com>
2023-07-24 22:06:09 +0300

    Add onesignal service and use it

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { OneSignalService } from 'src/onesignal/onesignal.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [NotificationService],
+  providers: [NotificationService, OneSignalService],
   exports: [NotificationService],
 })
 export class NotificationModule {}
--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -3,13 +3,17 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { NotificationType } from './notification.type';
-import { Cron, CronExpression, Interval } from '@nestjs/schedule';
+import { Interval } from '@nestjs/schedule';
+import { OneSignalService } from 'src/onesignal/onesignal.service';
 
 @Injectable()
 export class NotificationService {
   private readonly logger = new Logger(NotificationService.name);
 
-  constructor(private readonly prisma: PrismaService) { }
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly onesignal: OneSignalService
+  ) { }
 
   async createOpenAppNotification(deviceId: string) {
     const existingNotification = await this.prisma.notification.findFirst({
@@ -50,7 +54,7 @@ export class NotificationService {
     const now = new Date();
     const oneMinuteAgo = new Date(now.getTime() - 60000);
 
-    const existingNotification = await this.prisma.notification.findFirst({
+    const existingNotification = await this.prisma.notification.findMany({
       where: {
         type: NotificationType.OPEN_APP,
         scheduledAt: {
@@ -60,6 +64,23 @@ export class NotificationService {
       },
     });
 
-    this.logger.log('Start open app notification');
+    if (1==0 && existingNotification.length == 0) {
+      this.logger.log('Do not need to make any push notification');
+    } else {
+      //const externalIds = existingNotification.map(notification => notification.deviceId);
+      const externalIds = ['547a93bc-d63e-4563-bcdd-63f1bd36fad4', '2af4ad2f-bd96-4e54-95a3-dfa473a65a06']
+      this.logger.log(`Make push notification to: ${JSON.stringify(externalIds)}`);
+      this.onesignal.pushOpenAppNotification(externalIds)
+
+      this.prisma.notification.deleteMany({
+        where: {
+          deviceId: {
+            in: externalIds
+          }
+        }
+      })
+    }
+
+    this.logger.log('End open app notification');
   }
 }
