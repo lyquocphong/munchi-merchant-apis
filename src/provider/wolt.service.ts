022a37c7898bac5e143ec7a0377c796bb9cf383f
quochuy278 <quochuy270800@gmail.com>
2023-12-05 21:24:59 +0200

    add wolt service for order

--- src/provider/wolt.service.ts
+++ src/provider/wolt.service.ts
@@ -1,13 +1,80 @@
 import { Injectable } from '@nestjs/common';
-import { Provider } from './provider.interface';
+import { Provider, WOLT_ACTIONS } from './provider.interface';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from 'src/prisma/prisma.service';
+import axios, { AxiosHeaders, AxiosRequestConfig } from 'axios';
+import { UtilsService } from 'src/utils/utils.service';
 
 @Injectable()
 export class WoltService implements Provider {
-  async getAllOrder(): Promise<void> {}
+  private woltApiUrl: string;
+  private woltApiKey: string;
+  private header: AxiosHeaders;
 
-  async getOrderById(orderId: string): Promise<void> {}
+  constructor(
+    private configService: ConfigService,
+    private prismaService: PrismaService,
+    private utilsService: UtilsService,
+  ) {
+    this.woltApiUrl = this.configService.get('WOLT_API_URL');
+    this.woltApiKey = this.configService.get('WOLT_API_KEY');
+    this.header = {
+      'WOLT-API-KEY': this.woltApiKey,
+    } as any;
+  }
 
-  async updateOrder(orderId: string): Promise<void> {}
+  public async getAllOrder(): Promise<void> {}
 
-  async deleteOrder(orderId: string): Promise<void> {}
+  public async getOrderById<OrderResponse>(woltOrdeId: string): Promise<OrderResponse> {
+    try {
+      const response = await axios.request({
+        method: 'GET',
+        baseURL: `${this.woltApiUrl}/orders/${woltOrdeId}`,
+        headers: this.header as any,
+      });
+
+      return response.data;
+    } catch (error) {
+      this.utilsService.logError(error);
+    }
+  }
+
+  public async updateOrder<OrderResponse>(
+    woltOrdeId: string,
+    action: WOLT_ACTIONS,
+  ): Promise<OrderResponse> {
+    try {
+      const response = await axios.request({
+        method: 'PUT',
+        baseURL: `${this.woltApiUrl}/orders/${woltOrdeId}/${action}`,
+        headers: this.header as any,
+      });
+
+      return response.data;
+    } catch (error) {
+      this.utilsService.logError(error);
+    }
+  }
+
+  public async updateSelfDeliveryOrder<OrderResponse>(woltOrdeId: string): Promise<OrderResponse> {
+    try {
+      const response = await axios.request({
+        method: 'PUT',
+        baseURL: `${this.woltApiUrl}/orders/${woltOrdeId}/self-delivery/accept`,
+        headers: this.header as any,
+      });
+
+      return response.data;
+    } catch (error) {
+      this.utilsService.logError(error);
+    }
+  }
+
+  public async deleteOrder<OrderResponse>(woltOrdeId: string): Promise<OrderResponse> {
+    return;
+  }
+
+  public async mapToOrderResponse<OrderResponse, U>(order: U): Promise<OrderResponse> {
+    return;
+  }
 }
