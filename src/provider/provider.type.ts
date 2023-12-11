bc13c27668d461bba76136801cdd2bdc12c0014a
quochuy278 <quochuy270800@gmail.com>
2023-12-11 18:40:10 +0200

    rename provider.interface  to provider.type

---
+++ src/provider/provider.type.ts
@@ -0,0 +1,27 @@
+export interface Provider {
+  getAllOrder(): Promise<void>;
+
+  getOrderById<T>(orderId: string): Promise<T>;
+
+  updateOrder<T>(orderId: string, action: WOLT_ACTIONS): Promise<T>;
+
+  mapToOrderResponse<T, U>(order: U): Promise<T>;
+}
+
+/**
+ * This wolt actions take from wolt develop document to make it dynamic
+ *
+ * Reference: https://developer.wolt.com/docs/api/order
+ */
+export type WOLT_ACTIONS =
+  | 'accept'
+  | 'reject'
+  | 'ready'
+  | 'delivered'
+  | 'confirm-preorder'
+  | 'replace-items';
+
+export enum ProviderEnum {
+  Wolt = 'Wolt',
+  Munchi = 'Munchi',
+}
