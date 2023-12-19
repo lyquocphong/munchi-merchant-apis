d9fa79f68a9fbd7768f00ded2dc20c4151d4364f
quochuy278 <quochuy270800@gmail.com>
2023-12-19 18:52:58 +0200

    add api key strategy guard

---
+++ src/auth/guard/apiKey.guard.ts
@@ -0,0 +1,6 @@
+import { AuthGuard } from '@nestjs/passport';
+export class ApiKeyGuard extends AuthGuard('api-key') {
+  constructor() {
+    super();
+  }
+}
---
+++ src/auth/strategy/apiKey.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { SessionService } from '../session.service';
+import { HeaderAPIKeyStrategy } from 'passport-headerapikey';
+import { ApiKeyService } from '../apiKey.service';
+
+@Injectable()
+export class ApiKeyStrategy extends PassportStrategy(HeaderAPIKeyStrategy, 'api-key') {
+  constructor(private apiKeyService: ApiKeyService) {
+    super({ header: 'apiKey', prefix: '' }, true, async (apikey, done, req) => {
+      const checkKey = await apiKeyService.validateApiKey(apikey)
+      if (checkKey) {
+        return done(null, true);
+      }
+      return done(new UnauthorizedException(), null);
+    });
+  }
+}
