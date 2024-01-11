96c991a89c189bc1aaeb8bf99bf9df834fc163b9
quochuy278 <quochuy270800@gmail.com>
2024-01-11 23:10:55 +0200

    add order query validator

---
+++ src/order/filter/order.filter.ts
@@ -0,0 +1,29 @@
+import { IsArray, IsString } from 'class-validator';
+import { AvailableOrderStatus } from '../dto/order.dto';
+import { Transform } from 'class-transformer';
+import { AvailableProvider } from 'src/provider/provider.type';
+
+export class OrderStatusFilter {
+  @IsArray()
+  @Transform(({ value }) => {
+    try {
+      return JSON.parse(value);
+    } catch (error) {
+      return value;
+    }
+  })
+  providers: string[];
+
+  @IsString()
+  status: AvailableOrderStatus;
+
+  @IsArray()
+  @Transform(({ value }) => {
+    try {
+      return JSON.parse(value);
+    } catch (error) {
+      return value;
+    }
+  })
+  businessPublicIds: string[];
+}
