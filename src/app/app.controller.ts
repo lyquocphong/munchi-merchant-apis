6ddc8786aa99ad6f113f783f8da5b48b5b870d9a
quochuy278 <quochuy270800@gmail.com>
2024-02-08 16:11:44 +0200

    update app entry path

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -4,4 +4,9 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  welcome() {
+    return this.appService.getHello();
+  }
 }
