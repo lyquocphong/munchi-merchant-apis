f88b288c4dcd78121a6f72bc8c33f07c3b271afd
Huy Bui <quochuy270800@gmail.com>
2024-02-20 17:07:30 +0100

    create route to get order history

--- src/history/history.controller.ts
+++ src/history/history.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Request, UseGuards, ValidationPipe } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guard/jwt.guard';
+import { HistoryService } from './history.service';
+import { Historyquery } from './dto/history,dto';
 
+@UseGuards(JwtGuard)
 @Controller('history')
-export class HistoryController {}
+export class HistoryController {
+  constructor(private historyService: HistoryService) {}
+  @Get()
+  getOrderHistory(@Query(new ValidationPipe()) dateQuery: Historyquery, @Request() request: any) {
+    const { sessionPublicId } = request.user;
+    return this.historyService.getOrderHistory(sessionPublicId, dateQuery);
+  }
+}
