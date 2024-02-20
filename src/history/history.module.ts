8b2d32de61c28cd1c9d730fea6c55536ac2f687a
Huy Bui <quochuy270800@gmail.com>
2024-02-20 17:07:49 +0100

    imports necessary modules to history module

--- src/history/history.module.ts
+++ src/history/history.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-import { HistoryService } from './history.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { JwtStrategy } from 'src/auth/strategy/jwt.strategy';
+import { UserModule } from 'src/user/user.module';
 import { HistoryController } from './history.controller';
+import { HistoryService } from './history.service';
+import { OrderModule } from 'src/order/order.module';
+import { FinancialAnalyticsModule } from 'src/financial-analytics/financial-analytics.module';
 
 @Module({
-  providers: [HistoryService],
+  imports: [AuthModule, UserModule, OrderModule, FinancialAnalyticsModule],
+  providers: [HistoryService, JwtStrategy],
   controllers: [HistoryController],
 })
 export class HistoryModule {}
