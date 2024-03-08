ec3eb32d705c709533ce65031f7df69f7352112a
Huy Bui <quochuy270800@gmail.com>
2024-03-08 10:44:27 +0100

    add new spliting services to provider module as providers

--- src/provider/provider.module.ts
+++ src/provider/provider.module.ts
@@ -2,9 +2,34 @@ import { Module } from '@nestjs/common';
 import { OrderingService } from './ordering/ordering.service';
 import { ProviderManagmentService } from './provider-management.service';
 import { WoltService } from './wolt/wolt.service';
+import { WoltOrderMapperService } from './wolt/wolt-order-mapper';
+import { WoltRepositoryService } from './wolt/wolt-repository';
+import { WoltSyncService } from './wolt/wolt-sync';
+import { OrderingOrderMapperService } from './ordering/ordering-order-mapper';
+import { OrderingRepositoryService } from './ordering/ordering-repository';
 
 @Module({
-  providers: [ProviderManagmentService, OrderingService, WoltService],
-  exports: [OrderingService, WoltService, ProviderManagmentService],
+  providers: [
+    ProviderManagmentService,
+    // Ordering Services
+    OrderingService,
+    OrderingOrderMapperService,
+    OrderingRepositoryService,
+    // Wolt Services
+    WoltService,
+    WoltOrderMapperService,
+    WoltRepositoryService,
+    WoltSyncService,
+  ],
+  exports: [
+    OrderingService,
+    OrderingOrderMapperService,
+    OrderingRepositoryService,
+    WoltService,
+    ProviderManagmentService,
+    WoltOrderMapperService,
+    WoltRepositoryService,
+    WoltSyncService,
+  ],
 })
 export class ProviderModule {}
