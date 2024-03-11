f787e06888142cd0f3e76d969b840a9d7acf761d
quochuy278 <quochuy270800@gmail.com>
2024-03-11 17:36:16 +0200

    update menu module imports

--- src/menu/menu.module.ts
+++ src/menu/menu.module.ts
@@ -3,9 +3,12 @@ import { MenuService } from './menu.service';
 import { MenuController } from './menu.controller';
 import { ProviderModule } from 'src/provider/provider.module';
 import { JwtStrategy } from 'src/auth/strategy/jwt.strategy';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  providers: [MenuService, ProviderModule, JwtStrategy],
+  imports: [ProviderModule, AuthModule, UserModule],
+  providers: [MenuService, JwtStrategy],
   controllers: [MenuController],
 })
 export class MenuModule {}
