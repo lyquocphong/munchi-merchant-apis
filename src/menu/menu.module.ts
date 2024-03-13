7cb90aba96dc4e40dd7c8097466fd0233ab9f6a6
quochuy278 <quochuy270800@gmail.com>
2024-03-13 18:31:30 +0200

    add business module to menu module

--- src/menu/menu.module.ts
+++ src/menu/menu.module.ts
@@ -5,9 +5,10 @@ import { ProviderModule } from 'src/provider/provider.module';
 import { JwtStrategy } from 'src/auth/strategy/jwt.strategy';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserModule } from 'src/user/user.module';
+import { BusinessModule } from 'src/business/business.module';
 
 @Module({
-  imports: [ProviderModule, AuthModule, UserModule],
+  imports: [ProviderModule, AuthModule, UserModule, BusinessModule],
   providers: [MenuService, JwtStrategy],
   controllers: [MenuController],
 })
